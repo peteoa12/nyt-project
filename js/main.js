8718be1d55337f114fb69a2270b3c881eb2935af
peteoa12 <peter.norman@atlanta.creativecircus.edu>
2016-10-29 17:27:39 -0400

    First commit

--- js/main.js
+++ js/main.js
@@ -1,100 +1,89 @@
 
-var GoogleMapApi = (function(){
+var SimpleJson = (function() {
 
-  var map;
-  var searchField = document.getElementById("searchField");
-  var searchButton = document.getElementById("searchButton");
-  var placeResults = document.getElementById("place__results");
-  var myLatLng = {lat: 33.742712, lng: -84.338520}; // initial center point of map
-  var infowindow; 
-    
+  //-------GLOBAL VARS-----------------------//
 
-  function initMap() {
-    infowindow = new google.maps.InfoWindow();
-    map = new google.maps.Map(document.getElementById('map'), {
-      center: myLatLng,
-      zoom: 17
-    });
+  var $searchField = $("#searchField");
+  var $searchButton = $("#searchButton");
+  var $searchResults = $("#results");
+   var url = "https://api.nytimes.com/svc/search/v2/articlesearch.jsonp";
 
-    // searchButton.addEventListener("click", search);
-
-    searchButton.addEventListener("click", function(){
-      search();
-
-    });
-    
-    var marker = new google.maps.Marker({
-      position:myLatLng,
-      map: map,
-      title: 'My House',
-      animation: google.maps.Animation.DROP
-    });
-    
-    google.maps.event.addListener(marker, 'click', function() {
-        infowindow.setContent('My House');
-        infowindow.open(map, this);
-    });
-  };
-  
-  function createMarker(result) {
-    var marker = new google.maps.Marker({
-      position:result.geometry.location,
-      map: map,
-      title: result.name,
-      animation: google.maps.Animation.DROP
-    });
-    
-    google.maps.event.addListener(marker, 'click', function() {
-        createInfoWindow(result,marker);
-        infowindow.open(map, this);
-    });
-  };
-
-  function createInfoWindow(result, marker) {
-    var contentString = `<h3 class="marker-title">${result.name}<h3><div class="marker-address">${result.formatted_address}</div>`
-    infowindow.setContent(contentString);
-  };
-
-
-  function processPlacesResults(results, status) {
-    setSearchResult(results);
-    if (status == google.maps.places.PlacesServiceStatus.OK) {
-      for (var i = 0; i < results.length; i++) {
-        var result = results[i];
-        createMarker(result);
-      }
-    }
-  };
-
-
-  function search() {
-    var searchItem = document.getElementById("searchField").value;
-    var request = {
-      location: myLatLng,
-      radius: '5',
-      query: searchItem,
-      openNow:true
-    };
-
-    var service = new google.maps.places.PlacesService(map);
-    service.textSearch(request, processPlacesResults);
+//--------INIT-----------------------------//
+  function init(){
+    console.log("init");
+    setUpListeners();
   }
 
 
-  function setSearchResult(result) {
-    placeResults.innerHtml = "";
-    result.forEach(function(item){
-      var node = document.createElement("LI");
-      var textnode = document.createTextNode(item.name); 
-      node.innerHtml = result.name;
-      node.appendChild(textnode);
-      placeResults.appendChild(node);
-      console.log(item);
+//-------CLICK HANDLERS-------------------//
+  function setUpListeners() {
+      $searchButton.on('click', function(event) {
+        doSearchWithJsonP();
+        getArticleTitle();
+        return false;
+      });
+
+
+  }
+
+//-----DISPLAY ARTICLES--------------//
+
+  function displayArticles(data) {
+    console.log("success", data);
+  }
+
+//-------SEARCH ARTICLES-----------------//
+  function  doSearchWithJsonP() {
+    var searchItem = $("#searchField").val();    
+    url += '?' + $.param({
+      'api-key': "c05c3c0024ff4f3a882a8e0411838fb4",
+      'q': searchItem
+    });
+    
+    $.ajax({
+      url: url,
+      dataType:'jsonp',
+      jsonpCallback: 'svc_search_v2_articlesearch',
+      method: 'GET',
+    }).done(function(data) {
+      console.log('got data', data);
+    }).fail(function(err) {
+      throw err;
+    });
+
+ };
+
+  function  getArticleTitle() {
+    $.ajax({
+      url: url,
+      dataType:'jsonp',
+      jsonpCallback: 'svc_search_v2_articlesearch',
+      method:'GET',
     })
-  };
+    .done(displayArticleTitle) {
+      console.log("article title", data);
+    })
+    .fail(function(err) {
+      console.log("uh-oh!");
+    })
+    .always(function() {
+      console.log("complete");
+    });
+    
+  }
 
   return {
-    init: initMap
+    init:init
   };
 
-}());
+})();
+
+$(document).ready(function(){
+  SimpleJson.init();
+});
+
+
+
+
+
+
